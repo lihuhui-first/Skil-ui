6559497d5db9652b83ebe2375130e7273ebfd948
lihuihui <sxlsh1209@163.com>
2023-10-07 22:08:58 +0800

    Home页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,42 @@
 <template>
     <div>
-       <Topnav/>
+       <Topnav />
        <div class="banner">
           <h1>咕噜UI</h1>
           <h2>一个超级厉害的 UI 框架</h2>
           <p class="actions">
-            <a href="">GitHub</a>
-            <a href="">开始</a>
+            <a href="https://github.com">GitHub</a>
+            <router-link to="/doc">开始</router-link>
           </p>
        </div>
     </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue"
 export default {
-   components: {Topnav}
+   components: {Topnav},
 }
 </script>
 
 <style lang="scss" scoped>
-.topnav{
-   background: pink;
-   display: flex;
-   padding: 16px;
-   > .logo{
-      max-width: 6em;
-      margin-right: auto;
-   }
-   > .menu{
+   .banner {
+      padding: 100px 0;
       display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      > li{
-      margin: 0 1em;
-     }
-    }
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      background: rgb(144, 195, 144);
+      > .actions{
+         padding: 8px 0;
+         a {
+            margin: 0 8px;
+            background: #fff;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 10px;
+            }
+      }
    }
 </style>
