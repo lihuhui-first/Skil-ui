f7d22dfad62a1a333734f719ea736b9eaa105e34
lihuihui <sxlsh1209@163.com>
2023-10-15 14:57:28 +0800

    hover之后圆圈右滚

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,98 +1,98 @@
 <template>
-    <div>
-      <div class="layout">
-       <Topnav class="nav"/>
-       <div class="content">
+  <div>
+    <div class="layout">
+      <Topnav class="nav" />
+      <div class="content">
         <aside v-if="menuVisible">
-           <h2>组件列表</h2>
-           <ol>
+          <h2>组件列表</h2>
+          <ol>
             <li>
-                <router-link to="/doc/switch">Switch组件</router-link>
+              <router-link to="/doc/switch">Switch组件</router-link>
             </li>
             <li>
-                <router-link to="/doc/button">Button组件</router-link>
+              <router-link to="/doc/button">Button组件</router-link>
             </li>
             <li>
-                <router-link to="/doc/dialog">Dialog组件</router-link>
+              <router-link to="/doc/dialog">Dialog组件</router-link>
             </li>
             <li>
-                <router-link to="/doc/tabs">Tabs组件</router-link>
+              <router-link to="/doc/tabs">Tabs组件</router-link>
             </li>
-           </ol>
+          </ol>
         </aside>
         <main>
-            <router-view />
+          <router-view />
         </main>
-       </div>
-     </div>
+      </div>
     </div>
+  </div>
 </template>
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
-import { inject, Ref } from 'vue'
+import Topnav from "../components/Topnav.vue";
+import { inject, Ref } from "vue";
 
 export default {
-    components: { Topnav },
-    setup() {
-        const menuVisible = inject<Ref<boolean>>('menuVisible')
-        return {menuVisible}
-     }
-}
+  components: { Topnav },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    return { menuVisible };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .layout {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    > .nav {
-        flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
-    > .content {
-       flex-grow: 1;
-       padding-top: 60px;
-       padding-left: 156px;
-       @media(max-width:500px){
-           padding-left: 0;
-       }
-    }
+  }
 }
 .content {
-    display: flex;
-    > aside {
-        flex-shrink: 0;
-    }
-    > main {
-        flex-grow: 1;
-        padding: 16px;
-        background: lightgreen;
-    }
-}
-aside{
-    background: lightblue;
-    width: 150px;
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
     padding: 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding-top: 70px;
-    height: 100%;
-    > h2{
-        margin-bottom: 4px;
+    background: lightgreen;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
     }
-    > ol{
-        > li{
-            padding: 4px 0;
-        }
-    }
-    @media (max-width: 500px) {
+  }
+  @media (max-width: 500px) {
     position: fixed;
     top: 0;
     left: 0;
     padding-top: 70px;
   }
-    main {
-        overflow: auto;
-    }
+  main {
+    overflow: auto;
+  }
 }
 </style>
