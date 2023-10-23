e7fae85521802ac97d72313b0c0680741a4af758
lihuihui <sxlsh1209@163.com>
2023-10-23 22:14:57 +0800

    创建Dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dialog-overlay"></div>
+  //遮罩层
+  <div class="dialog-wrapper">
+    //本体
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>Ok</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: { Button },
+};
+</script>
