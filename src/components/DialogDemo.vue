5439a2fc7a151cf7f719dda00ae9f775891d7633
lihuihui <sxlsh1209@163.com>
2023-10-29 15:37:19 +0800

    接受visible属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,24 @@
 <template>
   <div>Dialog示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Dialog :visible="x"></Dialog>
+  <Button @click="toggle">toggle</Button>
 </template>
 <script lang="ts">
+import { ref } from "vue";
+import Button from "../lib/Button.vue";
 import Dialog from "../lib/Dialog.vue";
 export default {
   components: {
     Dialog,
+    Button,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    return { x, toggle };
   },
 };
 </script>
