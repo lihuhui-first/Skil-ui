6d6334634e121555dac7d7f18715160d72364a81
lihuihui <sxlsh1209@163.com>
2023-11-18 17:54:12 +0800

    完成indicator css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -10,11 +10,12 @@
       >
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
         class="gulu-tabs-content-item"
-        :class="{ selected: c.props.title === selected }"
+        :class="{ selected: c.props?.title === selected }"
         v-for="c in defaults"
         :is="c"
         :key="c"
@@ -58,6 +59,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -68,6 +70,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
