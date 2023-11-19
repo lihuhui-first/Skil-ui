e29073f0cb609fbfe1d71123ebb5d3f62c1f613e
lihuihui <sxlsh1209@163.com>
2023-11-19 14:38:39 +0800

    背景渐变

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,48 @@
 <template>
-    <div>
-       <Topnav />
-       <div class="banner">
-          <h1>咕噜UI</h1>
-          <h2>一个超级厉害的 UI 框架</h2>
-          <p class="actions">
-            <a href="https://github.com">GitHub</a>
-            <router-link to="/doc">开始</router-link>
-          </p>
-       </div>
+  <div class="topnavAndBanner">
+    <Topnav />
+    <div class="banner">
+      <h1>咕噜UI</h1>
+      <h2>一个超级厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
     </div>
+  </div>
 </template>
 <script lang="ts">
-import Topnav from "../components/Topnav.vue"
+import Topnav from "../components/Topnav.vue";
 export default {
-   components: {Topnav},
-}
+  components: { Topnav },
+};
 </script>
 
 <style lang="scss" scoped>
-   .banner {
-      padding: 100px 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      background: rgb(144, 195, 144);
-      > .actions{
-         padding: 8px 0;
-         a {
-            margin: 0 8px;
-            background: #fff;
-            $h: 28px;
-            height: $h;
-            line-height: $h;
-            border-radius: $h/2;
-            padding: 0 10px;
-            }
-      }
-   }
+.topnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(227, 255, 253, 1) 14%,
+    rgba(183, 233, 230, 1) 100%
+  );
+}
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 10px;
+    }
+  }
+}
 </style>
