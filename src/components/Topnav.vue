a5b3dd25213d6af912fdf5d4a3e400451e242ca6
lihuihui <sxlsh1209@163.com>
2023-11-27 22:14:09 +0800

    更新切换按钮样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,15 +6,24 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from "vue";
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
   setup() {
     const menuVisible = inject<Ref<boolean>>("menuVisible"); //get
     const toggleMenu = () => {
@@ -57,12 +66,13 @@ $color: #007974;
   > .toggleAside {
     width: 24px;
     height: 24px;
-    background: red;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
+    border-radius: 5px;
   }
   @media (max-width: 500px) {
     > .menu {
