837f62e6a768ab8a20386df21cfc17bdcfa4f179
lihuihui <sxlsh1209@163.com>
2023-12-14 22:50:32 +0800

    改造Button Demo

---
+++ src/components/Button4.demo.vue
@@ -0,0 +1,19 @@
+<demo>
+    支持disabled
+</demo>
+<template>
+  <div>
+    <Button theme="link" :disabled="true">禁用链接按钮</Button>
+    <Button theme="text" :disabled="true">禁用按钮</Button>
+    <Button :disabled="true">禁用按钮</Button>
+  </div>
+</template>
+
+<script>
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
