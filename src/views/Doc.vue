d3f5b0d2f141404dd35a5ff58c9ee59ff54df5b5
lihuihui <sxlsh1209@163.com>
2023-12-17 17:12:07 +0800

    优化官网，调整topnav顺序

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -57,6 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$aside-index: 10;
 .layout {
   display: flex;
   flex-direction: column;
@@ -92,6 +93,7 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
+  z-index: $aside-index;
   > h2 {
     padding: 0 16px;
   }
